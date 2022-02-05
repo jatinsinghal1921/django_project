6a08b8720aeb81f97ee393289e25efd89d2441d7
Jatin Singhal <Jsinghal@adobe.com>
2022-02-05 20:42:54 +0530

    Simple template render

---
+++ blog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        "author": "Jatin Singhal",
+        "title": "Post 1",
+        "content": "Post 1 Body Content",
+        "date_posted": "Feb 5, 2022"
+    },
+    {
+        "author": "Shubham Singhal",
+        "title": "Post 2",
+        "content": "Post 2 Body Content",
+        "date_posted": "Feb 6, 2022"
+    }
+]
+
+
+def home(request):
+    context = {
+        "posts": posts
+    }
+
+    return render(request, "blog/home.html", context)
+
+
+def about(request):
+    return render(request, "blog/about.html", {"title": "About"})
