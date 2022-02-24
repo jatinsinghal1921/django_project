26c184c73117694552f6e625ca56d7b78f5ad828
Jatin Singhal <Jsinghal@adobe.com>
2022-02-24 15:12:39 +0530

    Add authentication logic

--- users/views.py
+++ users/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 def registerUser(request):
-    formObj = RegisterForm()
+    if request.method == "POST":
+        formObj = RegisterForm(request.POST)
+        if formObj.is_valid():
+            print("User Created : " + formObj.cleaned_data["username"])
+            print("User email : " + formObj.cleaned_data["email"])
+            print("User password : " + formObj.cleaned_data["password1"])
+            formObj.save()
+            return redirect("blog-home")
+    else:
+        formObj = RegisterForm()
     return render(request, "users/register.html", {"form": formObj})
+
+
+@login_required(login_url='login')
+def profile(request):
+    return render(request, "users/profile.html")
